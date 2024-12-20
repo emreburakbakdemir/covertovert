9e511d708c7cc19cb32203552669b503e1274124
emreburakbakdemir <emreburakbakdemir38@gmail.com>
2024-12-20 22:03:25 +0300

    Solved the problems in connection. Successfully sends and recieves packets. No encoding included.

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -2,6 +2,11 @@ from CovertChannelBase import CovertChannelBase
 from scapy.all import IP, ICMP, sniff
 
 class MyCovertChannel(CovertChannelBase):
+    count = 0
+    flag = False
+    received_bits: list= []
+    decoded_message: list = []
+
     """
     - You are not allowed to change the file name and class name.
     - You can edit the class in any way you want (e.g. adding helper functions); however, there must be a "send" and a "receive" function, 
@@ -21,17 +26,33 @@ class MyCovertChannel(CovertChannelBase):
         
         Doesn't encode the binary message yet.
         """
-        
         binary_message = self.generate_random_binary_message_with_logging(log_file_name, message_len, message_len)
-        
+        binary_message += '00101110'
+        print(binary_message)
         for i in range(0, len(binary_message), bit_len):
             chunk = binary_message[i:i + bit_len]
             seq_number = int(chunk, 2)
             packet = IP(dst="172.18.0.3") / ICMP(seq = seq_number)
             CovertChannelBase.send(self, packet)
-        
+
             
         
+    def process_packet(self, packet):
+        if packet.haslayer(ICMP):
+            self.count += 1
+            seq_number: int = packet[ICMP].seq
+            self.received_bits.append(str(seq_number))
+            ascii_of_byte = ' '
+            if not (self.count % 8): 
+                string_of_byte = ''.join(self.received_bits[self.count-7:self.count])
+                int_of_byte = int(string_of_byte,2)
+                ascii_of_byte = chr(int_of_byte)
+                self.decoded_message.append(ascii_of_byte)
+            if ascii_of_byte == '.': self.flag = True
+            
+    def check_end(self, packet):
+        return self.flag
+    
     def receive(self, log_file_name, bit_length):
         """
         Sniffs packets and processes them chunk by chunk via using process_packet() function. Adds the sequence number
@@ -39,18 +60,14 @@ class MyCovertChannel(CovertChannelBase):
         
         No encoding/decoding
         """
-        received_bits = []
-           
-        def process_packet(packet):
-            if packet.haslayer(ICMP):
-                seq_number = packet[ICMP].seq
-                binary_chunk = format(seq_number, f'0{bit_length}b')
-                received_bits.append(binary_chunk)
-                if binary_chunk.endswith("."):
-                    return False
-            return True
+        
+        self.count = 0
 
-        sniff(filter="icmp", prn=process_packet, stop_filter=lambda pkt: not process_packet(pkt))
-        decoded_message = '.'.join(received_bits)
-        self.log_message(decoded_message, log_file_name)
+        sniff(
+            filter="icmp and icmp[icmptype] != icmp-echoreply", 
+            prn=self.process_packet,
+            stop_filter=self.check_end)
+            
+        self.decoded_message = ''.join(self.decoded_message)
+        self.log_message(self.decoded_message, log_file_name)
         
